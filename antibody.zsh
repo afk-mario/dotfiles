dbc3520160175d38a7bfb404277028a0c38c7b04
Mario Carballo Zama <afk@ellugar.co>
2020-05-23 16:08:47 -0500

    fix: zsh comments disappear
    zsh-syntax-highlighting was causing zsh comments to disappear on
    alacritty

--- antibody.zsh
+++ antibody.zsh
@@ -2,3 +2,6 @@ source <(antibody init)
 
 antibody bundle < ~/.dotfiles/zsh_plugins.txt
 
+# So comments show up, previously they were invisible (see
+# https://github.com/zsh-users/zsh-syntax-highlighting/issues/510).
+ZSH_HIGHLIGHT_STYLES[comment]='none'
