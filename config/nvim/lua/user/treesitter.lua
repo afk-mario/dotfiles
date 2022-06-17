da44aea7a5b77e1b9cfd1c75290c55c2727eb019
Mario Carballo Zama <afk@ellugar.co>
2022-06-17 14:24:24 -0500

    bugfix: disable treesitter for all and add manually langs

--- config/nvim/lua/user/treesitter.lua
+++ config/nvim/lua/user/treesitter.lua
@@ -1,7 +1,7 @@
 local tree = require 'nvim-treesitter.configs'
 
 tree.setup {
-    ensure_installed = "all",
+    ensure_installed = {"lua", "javascript", "bash", "css", "gdscript", "glsl", "go", "graphql", "html", "json", "typescript", "vim"},
     highlight = {enable = true, disable = {"javascript"}},
     indent = {enable = true},
     incremental_selection = {enable = true}
