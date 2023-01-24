3ebde112c3dba6379743ecc100db92f3279426cc
Mario Carballo Zama <afk@ellugar.co>
2023-01-23 23:48:49 -0600

    nvim: gdscript

---
+++ config/nvim/after/ftplugin/gdscript.lua
@@ -0,0 +1,8 @@
+-- setlocal tabstop=4
+vim.keymap.set("n", "<leader>r", vim.cmd.GodotRun, {buffer = true})
+vim.keymap
+	.set("n", "<leader>gr", vim.cmd.GodotRunFZF, {buffer = true})
+-- nnoremap <buffer> <F4> :GodotRunLast<CR>
+-- nnoremap <buffer> <F5> :GodotRun<CR>
+-- nnoremap <buffer> <F6> :GodotRunCurrent<CR>
+-- nnoremap <buffer> <F7> :GodotRunFZF<CR>
