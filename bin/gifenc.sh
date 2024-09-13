5120dbeac4e949e630f52fab370a3b9df21c3309
Mario Carballo Zama <afk@ellugar.co>
2024-09-13 14:02:23 -0600

    fix: improve gif encoding

--- bin/gifenc.sh
+++ bin/gifenc.sh
@@ -2,7 +2,7 @@
 # http://blog.pkh.me/p/21-high-quality-gif-with-ffmpeg.html#usage
 
 palette="/tmp/palette.png"
-filters="fps=15,scale=1280:-1:flags=lanczos"
+filters="fps=50,scale=iw*0.5:-1:flags=neighbor"
 
-ffmpeg -v warning -i $1 -vf "$filters,palettegen" -y $palette
+ffmpeg -v warning -i $1 -vf "$filters,palettegen=max_colors=4" -y $palette
 ffmpeg -v warning -i $1 -i $palette -lavfi "$filters [x]; [x][1:v] paletteuse" -y $2
