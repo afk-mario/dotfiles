9418193ea04d6c06e17259c862904c2a00763098
afk <afk@ellugar.co>
2025-10-13 13:29:52 -0600

    vim: complete mappings

--- config/nvim/after/plugin/complete.lua
+++ config/nvim/after/plugin/complete.lua
@@ -13,20 +13,20 @@ cmp.setup({
 		-- scroll up and down the documentation window
 		['<C-u>'] = cmp.mapping.scroll_docs(-4),
 		['<C-d>'] = cmp.mapping.scroll_docs(4),
-		['<C-j>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_jumpable() then
+		['<C-k>'] = cmp.mapping(function(fallback)
+			if luasnip.expand_or_jumpable() then
 				luasnip.expand_or_jump()
+			elseif cmp.visible() then
+				cmp.select_next_item()
 			else
 				fallback()
 			end
 		end, { 'i', 's' }),
-		['<C-k>'] = cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.jumpable(-1) then
+		['<C-j>'] = cmp.mapping(function(fallback)
+			if luasnip.jumpable(-1) then
 				luasnip.jump(-1)
+			elseif cmp.visible() then
+				cmp.select_prev_item()
 			else
 				fallback()
 			end
