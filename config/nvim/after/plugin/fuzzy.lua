c7c66c1ca715fdeab0bce992dc768a1a24d86236
afk <afk@ellugar.co>
2026-01-14 12:57:16 -0600

    vim: lines

--- config/nvim/after/plugin/fuzzy.lua
+++ config/nvim/after/plugin/fuzzy.lua
@@ -1,7 +1,7 @@
 vim.keymap.set('n', '<leader>pf', vim.cmd.Files)
 vim.keymap.set('n', '<leader>pb', vim.cmd.Buffers)
 vim.keymap.set('n', '<leader>pg', vim.cmd.Rg)
-vim.keymap.set('n', '<leader>pl', vim.cmd.Lines)
+vim.keymap.set('n', '<leader>pl', vim.cmd.BLines)
 vim.keymap.set('n', '<leader>pc', vim.cmd.Colors)
 vim.keymap.set('n', '<leader>pr', vim.cmd.History)
 
